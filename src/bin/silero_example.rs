39eaaa5f328174c13867cdbc1a7c67eada77531f
Oliver-Pikett <oliver.pikett@infoserv.ltd>
2024-02-22 10:42:48 +0000

    Update
    Silero now processing full audio files

--- src/bin/silero_example.rs
+++ src/bin/silero_example.rs
@@ -3,11 +3,16 @@
 #![allow(unused_mut)]
 #![allow(dead_code)]
 
+use anyhow::Error;
 use clap::Parser;
 
-use ndarray::{array, concatenate, s, Array1, Axis, CowArray, IxDyn};
+use ndarray::{arr1, arr2, arr3, Array2, Array3};
+use ndarray::{array, concatenate, s, Array1, Axis, CowArray, IxDyn, FixedInitializer};
 
+use rand::seq::index::sample;
 use rodio::{decoder::DecoderError, Decoder, Sample, Source};
+use id3::Tag;
+use hound::WavReader;
 
 use ort::inputs;
 use ort::GraphOptimizationLevel;
@@ -17,13 +22,18 @@ use ort::{CoreMLExecutionProvider, Session, Tensor};
 
 use rand::Rng;
 
+use std::array;
 use std::convert::TryInto;
-use std::error::Error;
+use std::error::Error as Err;
 use std::fs::File;
 use std::io::BufReader;
+use std::any::type_name;
+use std::process::Output;
 
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
+
+use plotters::prelude::*;
 
 
 #[derive(Parser, Debug)]
@@ -38,195 +48,93 @@ pub struct Args {
     pub audio: String,
 }
 
-/*
-fn main() {
+pub struct AudioInfo {
 
-    let audio_sample_result = get_audio("/Users/oliver.pikett/Documents/SileroDev/my_work/files/10-seconds.mp3");
-
-    match audio_sample_result {
-
-        Ok(audio_sample) => {
-
-            println!("Running langauge model");
-            let program_result = run(audio_sample);
-            match program_result {
-
-                Ok(program_ran) => {
-                    println!("Program ran sucessfully");
-                }
-
-                Err(err) => {
-                    println!("~~~Language model encounted an error:");
-                    println!("{}~~~", err);
-                    std::process::exit(1);
-                }
-            }
-        }
-
-        Err(err) => {
-            std::process::exit(1);
-        }
-    }
-
+    sample_rate: i64,
+    audio_array: Vec<f32>,
 }
 
-fn get_audio(file_path: &str) -> Result<Vec<i16>, Box<dyn Error>> {
-
-    let aud_file: File = File::open(file_path)?;
-    let aud_decoder: Decoder<BufReader<File>> = Decoder::new(BufReader::new(aud_file))?;
-    let aud_array: Vec<i16> = aud_decoder.collect();
-    Ok(aud_array)
-}
-
-fn run(data_sample: Vec<i16>) -> Result<(), Box<dyn Error>> {
-
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
-        .finish();
-
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
-    println!("~~~Initalising Environment~~~");
-    let environment = Environment::builder()
-        .with_name("test")
-        // The ONNX Runtime's log level can be different than the one of the wrapper crate or the application.
-        .with_log_level(LoggingLevel::Info)
-        .build()?;
-
-    println!("~~~Initalising Session~~~");
-    let mut session = environment
-
-        .new_session_builder()?
-        .with_optimization_level(GraphOptimizationLevel::Basic)?
-        .with_number_threads(1)?
-        .with_model_from_file("/Users/oliver.pikett/Documents/SileroDev/my_work/files/silero_vad.onnx")?;
-        //.with_model_from_file("/Users/oliver.pikett/Documents/SileroDev/my_work/files/alexnet_Opset18.onnx")?;
-
-    println!("~~~Session successfully initialised~~~");
-
-    let input_info = &session.inputs;
-    for input in input_info {
-        println!("Input Name: {}", input.name);
-        println!("Input Type: {:?}", input.dimensions);
-    }
-
-    Ok(())
-
-}
-*/
 
 fn main() -> Result<(), anyhow::Error>{
 
-    let args = Args::parse();
+    let args = Args::parse(); 
 
-    let silero_info =
-        get_model_info(&args.model)?;
-    
-
-    let audio_sample_result =
-        get_audio(&args.audio)?;
-
-    let model_result = run_model(&args.model, &audio_sample_result)?;
+    let model_result = run_model(&args.model, &args.audio)?;
   
     Ok(())
 }
 
-fn get_audio(file_path: &str) -> Result<Vec<f32>, anyhow::Error> {
+fn get_audio_mp3(file_path: &str) -> Result<Vec<f32>, anyhow::Error> {
+
     let aud_file = File::open(file_path)?;
     let aud_decoder = Decoder::new(BufReader::new(aud_file))?;
     let aud_array: Vec<i16> = aud_decoder.collect();
-    
-    // you can do away with this by tagging a map() conversion onto the above
-    let mut return_arr = Vec::new();
-    for item in aud_array {
-        return_arr.push(item as f32);
-    }
+    let aud_array: Vec<f32> = aud_array.iter().map(|&x| x as f32).collect();
 
-    Ok(return_arr)
+    /*match Tag::read_from_path(file_path) {
+        Ok(tag) => {
+            if let Some(sr) = tag.sample_rate() {
+                sample_rate = sr;
+            } else {
+                return Error(println!("File has no metadata"));
+            }
+        }
+        Err(err) => {
+            eprintln!("Error reading MP3 file metadata: {}", err);
+        }
+    }*/
+
+
+
+    Ok(aud_array)
 }
 
-fn display_element_type(t: TensorElementDataType) -> &'static str {
-    match t {
-        TensorElementDataType::Bfloat16 => "bf16",
-        TensorElementDataType::Bool => "bool",
-        TensorElementDataType::Float16 => "f16",
-        TensorElementDataType::Float32 => "f32",
-        TensorElementDataType::Float64 => "f64",
-        TensorElementDataType::Int16 => "i16",
-        TensorElementDataType::Int32 => "i32",
-        TensorElementDataType::Int64 => "i64",
-        TensorElementDataType::Int8 => "i8",
-        TensorElementDataType::String => "str",
-        TensorElementDataType::Uint16 => "u16",
-        TensorElementDataType::Uint32 => "u32",
-        TensorElementDataType::Uint64 => "u64",
-        TensorElementDataType::Uint8 => "u8",
-    }
+fn get_audio_wav(file_path: &str) -> Result<AudioInfo, anyhow::Error> {
+
+    let mut reader = WavReader::open(file_path)?;
+    let samp_rate: i64 = reader.spec().sample_rate as i64;
+    let aud_array: Vec<i32> = reader.samples::<i32>().map(|x| x.unwrap()).collect::<Vec<_>>();
+    let aud_array: Vec<f32> = aud_array.iter().map(|&x| x as f32).collect();
+    let return_aud: AudioInfo = AudioInfo {
+        sample_rate: samp_rate,
+        audio_array: aud_array,
+    };
+
+    Ok(return_aud)
 }
+fn run_model(model_location: &str, audio_location: &str) -> ort::Result<()> {
 
-// TODO: Fix this function
-// the issue with teh latest (v2) ort crate is ort::value appears to be private
-//fn display_value_type(value: &ort::value::ValueType) -> String {
-//     match value {
-//         ValueType::Tensor { ty, dimensions } => {
-//             format!(
-//                 "Tensor<{}>({})",
-//                 display_element_type(*ty),
-//                 dimensions
-//                     .iter()
-//                     .map(|c| if *c == -1 {
-//                         "dyn".to_string()
-//                     } else {
-//                         c.to_string()
-//                     })
-//                     .collect::<Vec<_>>()
-//                     .join(", ")
-//             )
-//         }
-//         ValueType::Map { key, value } => format!(
-//             "Map<{}, {}>",
-//             display_element_type(*key),
-//             display_element_type(*value)
-//         ),
-//         ValueType::Sequence(inner) => format!("Sequence<{}>", display_value_type(inner)),
-//     }
-// }
+    let audio_data = get_audio_wav(audio_location).unwrap();
+    let audio_vec = audio_data.audio_array;
+    let mut no_of_windows = (audio_vec.len() / 1536) as i16;
+    let len_final_window = (audio_vec.len() % 1536) as i16;
+    let mut audio_windows: Vec<Vec<f32>> = Vec::new();
+    //could put this in a method?
+    if no_of_windows == 0 {
+        audio_windows.push(audio_vec.clone());
 
-fn get_model_info(model_path: &str) -> Result<(), anyhow::Error> {
-    let session = Session::builder()?.with_model_from_file(model_path)?;
-    let mut model_info = String::new();
-    let meta = session.metadata()?;
-    if let Ok(x) = meta.name() {
-        model_info.push_str(&format!("Name: {}\n", x));
-    }
-    if let Ok(x) = meta.description() {
-        model_info.push_str(&format!("Description: {}\n", x));
-    }
-    if let Ok(x) = meta.producer() {
-        model_info.push_str(&format!("Produced by {}\n", x));
+    } else {
+
+        //Works up to 74 hours
+        let mut current_start: usize = 0;
+        for i in 0..no_of_windows {
+            let mut temp_vec: Vec<f32> = Vec::new();
+            let end_index: usize = current_start + 1536;
+            let window = &audio_vec[current_start..end_index];
+            temp_vec.extend_from_slice(window);
+            audio_windows.push(temp_vec);
+            current_start = end_index;
+        }
+
+        if len_final_window != 0 {
+            let mut temp_vec: Vec<f32> = Vec::new();
+            let end_index: usize = audio_vec.len() as usize;
+            let window = &audio_vec[current_start..end_index];
+            temp_vec.extend_from_slice(window);
+            audio_windows.push(temp_vec);
+        }
     }
 
-    model_info.push_str("Inputs:\n");
-    for (i, input) in session.inputs.iter().enumerate() {
-        model_info.push_str(&format!("   {}", i.to_string()));
-        model_info.push_str(&format!(" {}:", input.name));
-
-        // TODO: Fix this
-        // model_info.push_str(&format!(" {}\n", display_value_type(&input.input_type)));
-    }
-    model_info.push_str("Outputs:\n");
-    for (i, output) in session.outputs.iter().enumerate() {
-        model_info.push_str(&format!("   {}", i.to_string()));
-        model_info.push_str(&format!(" {}:", output.name));
-        // TODO: Fix this
-        // model_info.push_str(&format!(" {}\n", display_value_type(&output.output_type)));
-    }
-    println!("{}", model_info.to_string());
-
-    Ok(())
-}
-
-fn run_model(model_location: &str, data_sample: &Vec<f32>) -> ort::Result<()> {
     ort::init()
         .with_name("Silero-VAD")
         .with_execution_providers([CoreMLExecutionProvider::default().build()])
@@ -237,47 +145,112 @@ fn run_model(model_location: &str, data_sample: &Vec<f32>) -> ort::Result<()> {
         .with_intra_threads(1)?
         .with_model_from_file(model_location);
 
-    let mut input_array = Array1::from_iter(data_sample.iter().cloned());
-    let input_array2 = input_array.view().insert_axis(Axis(0));
 
-    let sr: i64 = 8000;
+    let mut input_audio_data = Array1::from_iter(audio_vec.iter().cloned());
+    let input_audio_data = input_audio_data.view().insert_axis(Axis(0));
+
+    let sr: i64 = audio_data.sample_rate;
+    println!("{}", sr);
     let sr_array = Array1::from(vec![sr]);
+    
+    let mut h = make_h_c_array();
+    let mut c = make_h_c_array();
+    let dims = h.raw_dim();
+    
+    let mut outputs: Vec<Vec<f32>> = Vec::new();
+    let session_binding = session?;
 
-    let arr_1: Array1<f32> = array![
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-    ];
-    let arr_1 = arr_1.insert_axis(ndarray::Axis(0));
-    //let cow_arr: CowArray<'_, f32, IxDyn> = CowArray::from(arr_1);
+    for i in 0..audio_windows.len() {
 
-    let arr_2 = array![arr_1.clone(), arr_1.clone()];
+        let mut current_window: Vec<f32> = audio_windows.get(i).unwrap().clone();
+        let mut current_window = Array1::from_iter(current_window.iter().cloned());
+        let current_window = current_window.view().insert_axis(Axis(0));
+        let temp_sr = sr_array.clone();
+        let temp_h = h.clone();
+        let temp_c = c.clone();
 
-    let h = arr_2.clone();
-    let c = arr_2.clone();
+        let output = session_binding.run(inputs![current_window, temp_sr, temp_h, temp_c]?)?;
 
-    //let c_array = Array1::from(vec![c]);
+        let window_output: Tensor<f32> = output["output"].extract_tensor()?;
+        let hn: Tensor<f32> = output["hn"].extract_tensor()?;
+        let cn: Tensor<f32> = output["cn"].extract_tensor()?;
 
-    let binding = session?;
-    //not progressing past this line
-    // TODO: uncomment the lines below
-    // let outputs = binding.run(inputs![input_array2, sr_array, h, c]?)?;
-    // let generated_tokens: Tensor<f32> = outputs["output1"].extract_tensor()?;
-    // let generated_tokens = generated_tokens.view();
-    // println!("{}", generated_tokens.get(0).unwrap());
+        let window_output: ort::ArrayViewHolder<'_, f32> = window_output.view();
+        let window_output: Vec<f32> = window_output.iter().copied().collect();
+        outputs.push(window_output);
+
+        let hn = hn.view();
+        let cn = cn.view();
+
+        let hn: Vec<f32> = hn.iter().copied().collect();
+        let cn: Vec<f32> = cn.iter().copied().collect();
+
+        h = Array3::from_shape_vec((dims[0], dims[1], dims[2]), hn).unwrap();
+        c = Array3::from_shape_vec((dims[0], dims[1], dims[2]), cn).unwrap();
+
+    }
+    
+    let outputs: Vec<f32> = outputs.into_iter().flat_map(|f| f ).collect();
+    println!("{:?}", outputs);
 
     Ok(())
+    
 }
 
-fn create_rand_array(length: i32) -> Vec<(f32, f32)> {
-    let mut arr: Vec<(f32, f32)> = Vec::new();
-    let mut rng = rand::thread_rng();
+fn make_h_c_array() -> Array3<f32> {
 
-    for i in 0..length {
-        let int1: f32 = rng.gen_range(0..1000) as f32;
-        let int2: f32 = rng.gen_range(0..10000) as f32;
-        arr.push((int1, int2));
+    let a = Array3::<f32>::zeros((2, 1, 64));
+    return a;
+}
+
+fn create_graphs(input_audio: &Vec<f32>, output: &Vec<f32>) -> Result<(), anyhow::Error> {
+
+    let mut max: f32 = 0.0;
+    let mut graph_input_vec: Vec<f32> = Vec::new();
+    let mut graph_output_vec: Vec<f32> = Vec::new();
+
+    for i in input_audio {
+        if i > &max {
+            max = *i;
+        }
     }
-    return arr;
+
+    for i in 0..input_audio.len() {
+
+        let mut temp: f32 = *graph_input_vec.get(i).unwrap();
+        temp = temp/&max;
+        graph_input_vec.push(temp);
+    }
+
+    for i in output {
+        for j in 0..1536 {
+            graph_output_vec.push(i.clone());
+        }
+    }
+
+    if graph_input_vec.len() > graph_output_vec.len() {
+
+        graph_output_vec.truncate(graph_input_vec.len());
+    }
+
+    let root = BitMapBackend::new("waveform.png", (800, 600)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let max_value = 1;
+    let min_value = 0;
+
+    let mut chart = ChartBuilder::on(&root)
+    .x_label_area_size(30)
+    .y_label_area_size(30)
+    .margin(5)
+    .caption("Waveform", ("Arial", 30).into_font())
+    .build_cartesian_2d(0..graph_input_vec.len() as i32, 0..1)?;
+
+    chart.draw_series(LineSeries::new(
+        (0..input_audio.len()).map(|i| (i as i32, samples[i])),
+        &RED,
+    ))?;
+
+
+    Ok(())
 }
