747a328f68c2e0b2aa830eadadf38118db794b68
Oliver-Pikett <oliver.pikett@infoserv.ltd>
2024-03-28 19:36:12 +0000

    Response to more comments
    Responded to more comments left on the code review most noteworthy being adding an additional method to for wav files to be processed rejecting any with an incompatible sample rate

---
+++ src/bin/demo.rs
@@ -0,0 +1,11 @@
+/*use crate::vad_session::VadSession;
+use crate::vad_environment::VadEnvironment;
+
+fn main() {
+
+    let audio_paths: Vec<String> = Vec::new();
+
+    let vad_environment = VadEnvironment::new(("files/silero_vad.onnx").to_string());
+    vad_environment.new
+}
+*/
